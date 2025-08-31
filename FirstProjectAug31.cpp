e53e5476c57fdfc0e09ca20a4e727f9c236a0a75
simon <simon@ALEXCOMPUTER>
2025-08-31 19:52:45 -0400

    Add project files.

---
+++ FirstProjectAug31.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char firstinitial;
+	char middleinitial;
+        char lastinitial;
+		cout << "Please enter the initial of your first name: ";
+	cin >> firstinitial;
+	cout << "Please enter the initial of your middle name: ";
+	cin >> middleinitial;
+	cout << "Please enter the initial of your last name: ";
+	cin >> lastinitial;
+	cout << "Your initials are " << firstinitial << middleinitial << lastinitial;
+
+}
